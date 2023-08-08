c773a82a02d2c2ba6189477488c4c54b4bd1f36d
Kareem <kareem@MacBook-Air-von-Kareem.local>
2023-08-08 15:15:40 +0200

    login/register api composition

--- pages/login.vue
+++ pages/login.vue
@@ -2,108 +2,45 @@
   <div class="flex items-center justify-center min-h-screen bg-gray-100">
     <div class="w-96 bg-white p-8 shadow-lg rounded-lg">
       <h1 class="text-3xl font-semibold mb-6">Login</h1>
-      <form>
-        <div class="mb-4">
-          <label for="email" class="block text-sm font-medium text-gray-700">E-Mail</label>
-          <input
-            id="email"
-            type="email"
-            class="mt-1 focus:ring focus:border-blue-300 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-          />
-        </div>
-        <div class="mb-4">
-          <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
-          <input
-            id="password"
-            type="password"
-            class="mt-1 focus:ring focus:border-blue-300 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-          />
-        </div>
-        <div class="mt-6">
-          <button
-            type="submit"
-            class="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
-          >
-            Log In
-          </button>
-        </div>
-      </form>
-      <div class="mt-6">
-        <p class="text-gray-700">Not registered yet?</p>
-        <button
-          type="button"
-          class="w-full bg-green-500 text-white py-2 px-4 rounded-md mt-2 hover:bg-green-600 focus:outline-none focus:ring focus:border-green-300"
-          @click="showRegisterForm"
-        >
-          Register
-        </button>
-      </div>
-      <!-- Registration form (hidden by default) -->
-      <form v-if="showRegistrationForm" class="mt-6">
-        <div class="mb-4">
-          <label for="firstName" class="block text-sm font-medium text-gray-700">First Name</label>
-          <input
-            id="firstName"
-            type="text"
-            class="mt-1 focus:ring focus:border-blue-300 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-          />
-        </div>
-        <div class="mb-4">
-          <label for="lastName" class="block text-sm font-medium text-gray-700">Last Name</label>
-          <input
-            id="lastName"
-            type="text"
-            class="mt-1 focus:ring focus:border-blue-300 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-          />
-        </div>
-        <div class="mb-4">
-          <label for="regEmail" class="block text-sm font-medium text-gray-700">E-Mail</label>
-          <input
-            id="regEmail"
-            type="email"
-            class="mt-1 focus:ring focus:border-blue-300 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-          />
-        </div>
-        <div class="mb-4">
-          <label for="regPassword" class="block text-sm font-medium text-gray-700">Password</label>
-          <input
-            id="regPassword"
-            type="password"
-            class="mt-1 focus:ring focus:border-blue-300 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-          />
-        </div>
-        <div class="mb-4">
-          <label for="confirmPassword" class="block text-sm font-medium text-gray-700">Confirm Password</label>
-          <input
-            id="confirmPassword"
-            type="password"
-            class="mt-1 focus:ring focus:border-blue-300 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-          />
-        </div>
-        <div class="mt-6">
-          <button
-            type="submit"
-            class="w-full bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600 focus:outline-none focus:ring focus:border-green-300"
-          >
-            Register
-          </button>
-        </div>
+      <form @submit.prevent="login">
+        <input v-model="loginData.email" type="email" placeholder="E-Mail" class="input" />
+        <input v-model="loginData.password" type="password" placeholder="Password" class="input" />
+        <button type="submit" class="button-blue">Log In</button>
       </form>
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      showRegistrationForm: false,
-    };
-  },
-  methods: {
-    showRegisterForm() {
-      this.showRegistrationForm = true;
-    },
-  },
+<script setup>
+import { ref } from 'vue';
+
+const showRegistrationForm = ref(false);
+const loginData = ref({
+  email: '',
+  password: '',
+});
+
+const showRegisterForm = () => {
+  showRegistrationForm.value = true;
 };
+
+const login = () => {
+  console.log('Login data:', loginData.value);
+  // Perform login logic using loginData.email and loginData.password
+};
+
 </script>
+
+<style scoped>
+.input {
+  @apply border rounded p-2 mb-4 w-full text-sm focus:outline-none focus:border-blue-300;
+}
+
+.button-blue {
+  @apply bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300;
+}
+
+.button-blue:hover {
+  @apply bg-blue-600;
+}
+</style>
---
+++ pages/register.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="flex items-center justify-center min-h-screen bg-gray-100">
+      <div class="w-96 bg-white p-8 shadow-lg rounded-lg">
+        <h1 class="text-3xl font-semibold mb-6">Register</h1>
+        <form @submit.prevent="register">
+          <input v-model="registrationData.firstName" type="text" placeholder="First Name" class="input" />
+          <input v-model="registrationData.lastName" type="text" placeholder="Last Name" class="input" />
+          <input v-model="registrationData.email" type="email" placeholder="E-Mail" class="input" />
+          <input v-model="registrationData.password" type="password" placeholder="Password" class="input" />
+          <input v-model="registrationData.confirmPassword" type="password" placeholder="Confirm Password" class="input" />
+          <button type="submit" class="button-blue">Register</button>
+        </form>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  
+  const showRegistrationForm = ref(false);
+  const registrationData = ref({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+  
+  const showLoginForm = () => {
+    showRegistrationForm.value = false;
+  };
+  
+  const register = () => {
+    console.log('Registration data:', registrationData.value);
+    // Perform registration logic using registrationData
+  };
+  
+  </script>
+  
+  <style scoped>
+  .input {
+    @apply border rounded p-2 mb-4 w-full text-sm focus:outline-none focus:border-blue-300;
+  }
+  
+  .button-blue {
+    @apply bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300;
+  }
+  
+  .button-blue:hover {
+    @apply bg-blue-600;
+  }
+  </style>
+  
